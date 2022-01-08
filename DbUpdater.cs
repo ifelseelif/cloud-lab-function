09ac2f65b0ce2e4dcbd41835118aa7570fdb3d52
Artem Kolokolov <yougrysha@gmail.com>
2022-01-08 20:14:18 +0300

    updated crontab time

--- DbUpdater.cs
+++ DbUpdater.cs
@@ -10,14 +10,15 @@ namespace Db_Trigger
     public class DbUpdater
     {
         [FunctionName("Function1")]
-        public async Task RunAsync([TimerTrigger("*/30 * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task RunAsync([TimerTrigger("* */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+            var client = new HttpClient();
+            var response = await client.GetAsync("http://www.contoso.com/");
             response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
+            var responseBody = await response.Content.ReadAsStringAsync();
             log.LogInformation($"Test body: {responseBody}");
+            log.LogInformation("Ok");
         }
     }
 }
